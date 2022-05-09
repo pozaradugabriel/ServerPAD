8b27c792e90993581ce9cfd733b26731b75c03aa
Pantea Daniel <daniel.pantea@eurosysthem.ro>
2022-05-09 22:50:07 +0300

    Project Setup with dependencies

---
+++ src/main/java/com/artexplorer/proiectpad/ArtExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.artexplorer.proiectpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artexplorer/proiectpad/ArtExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artexplorer.proiectpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
